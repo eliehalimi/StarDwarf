b0b3df39cfdbf74087062906e18d9c61cecd29d6
Elie Halimi <elie.halimi@epita.fr>
2018-02-18 15:17:03 +0100

    DrawCircle works correctly is better written, can customize it easily

--- SDL/circle.c
+++ SDL/circle.c
@@ -3,74 +3,76 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SCR_WDT 1280
 #define SCR_HGT 960
 #define M_PI	3.14159265358979323846264338327950288
+
 const int SCR_CEN_X = SCR_WDT / 2;
 const int SCR_CEN_Y = SCR_HGT / 2;
 
-int DrawCircle( SDL_Renderer *r, int shiftx, int shifty );
-char* getline(void);
-int main ( int argc, char *argv[] )
+
+int DrawCircle( SDL_Renderer *r, int x, int y, int radius);
+
+int main ()
 {
-    SDL_Init ( SDL_INIT_VIDEO ); 
-    SDL_Window *window = SDL_CreateWindow ( "Drawing a Circle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCR_WDT, SCR_HGT, 0 );
-    SDL_Renderer *renderer = SDL_CreateRenderer ( window, -1, SDL_RENDERER_SOFTWARE );
-    char * c;
-    int is_running = 1;
-    int x = 0;
-    if (argc > 2)
-	    is_running = 0;
-    
-    int y = 0;
-    while ( is_running && x+ SCR_CEN_X < SCR_WDT)
-    {
+       	SDL_Init ( SDL_INIT_VIDEO ); 
+    	SDL_Window *window = SDL_CreateWindow ( "Drawing a Circle", SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED, SCR_WDT, SCR_HGT, 0 );
+    	SDL_Renderer *renderer = SDL_CreateRenderer ( window, -1, SDL_RENDERER_SOFTWARE );
+   	
+   	int is_running = 1;
+	
 	SDL_Event event;
-        if ( SDL_PollEvent( &event ))
-        {
-            if ( event.type == SDL_QUIT )
-            {
-                is_running = 0;
-            }   
-        }	
-        SDL_RenderClear ( renderer );
-	//TO MOVE CIRCLE FROM LEFT TO RIGHT:
-	//x += 1;
-        y = DrawCircle( renderer, x, y );
-	SDL_SetRenderDrawColor ( renderer, 0, 0, 0, 255 );
-        SDL_RenderPresent ( renderer );
-
-    }
-
-    SDL_Quit ();
-
-return 0; 
+      	while (is_running)
+	{
+		if ( SDL_PollEvent( &event ))
+        	{
+           		if ( event.type == SDL_QUIT )
+            		{
+                		is_running = 0;
+            		}   
+       	 	}	
+        
+		SDL_RenderClear ( renderer );
+		DrawCircle( renderer, SCR_CEN_X, SCR_CEN_Y, 200);
+		SDL_SetRenderDrawColor ( renderer, 0, 0, 0, 255 );
+        	SDL_RenderPresent ( renderer );
+	}
+    	SDL_Quit ();
+	return 0;
 }
 
-int DrawCircle( SDL_Renderer *r, int shiftx, int shifty)
+/*
+** DrawCircle : Draws a circle at position x and y
+** with a given radius
+*/ 
+
+
+int DrawCircle( SDL_Renderer *r, int x, int y, int radius)
 {
-    int radius = 40; 
-    int new_x = 0+shiftx;
+    int new_x = 0;
     int new_y = 0;
-    int old_x = shifty;
-    int old_y = SCR_CEN_Y;
+    int old_x = x + radius;
+    int old_y = y;
     float step = (M_PI * 2) / 50; 
 
     SDL_SetRenderDrawColor ( r, 255, 255, 255, 255 );
 
     for ( float theta = 0; theta <= (M_PI * 2); theta += step )
     {
-        new_x = SCR_CEN_X +shiftx+ (radius * cos ( theta ));
-        new_y = SCR_CEN_Y - (radius * sin ( theta ));   
-
-        SDL_RenderDrawLine ( r, old_x, old_y, new_x, new_y);
+        new_x = x + (radius * cos (theta));
+	new_y = y - (radius * sin (theta));
+        
+	SDL_RenderDrawLine ( r, old_x, old_y, new_x, new_y);
 
         old_x = new_x;
         old_y = new_y;
     }
 
-    new_x =  SCR_CEN_X +shiftx+ (radius * cos ( 0 ));
-    new_y =  SCR_CEN_Y - (radius * sin ( 0 ));  
+    new_x = x + (radius * cos( 0 ));
+    new_y = y - (radius * sin( 0 ));
+    
     SDL_RenderDrawLine( r, old_x, old_y, new_x, new_y );
 	
     return old_x;
