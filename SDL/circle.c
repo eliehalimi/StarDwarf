a62bc320c69cdc22222e42f9374b9ba041e41f62
Elie Halimi <elie.halimi@epita.fr>
2018-02-18 17:29:07 +0100

    added a background to GUI

--- SDL/circle.c
+++ SDL/circle.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL_image.h>
 
 #define SCR_WDT 1280
 #define SCR_HGT 960
@@ -16,12 +17,30 @@ int DrawCircle( SDL_Renderer *r, int x, int y, int radius);
 
 int main ()
 {
-       	SDL_Init ( SDL_INIT_VIDEO ); 
+	SDL_Texture *background = NULL;
+	
+      	SDL_Init ( SDL_INIT_VIDEO ); 
+
     	SDL_Window *window = SDL_CreateWindow ( "Drawing a Circle", SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED, SCR_WDT, SCR_HGT, 0 );
     	SDL_Renderer *renderer = SDL_CreateRenderer ( window, -1, SDL_RENDERER_SOFTWARE );
    	
-   	int is_running = 1;
+	int w, h;
+
+
+	// Loading image
+	background = IMG_LoadTexture(renderer, "../resources/bg.jpg");
+	SDL_QueryTexture(background, NULL, NULL, &w, &h);
+	SDL_SetRenderTarget(renderer, background);
+
+	// Scaling image
+	SDL_Rect texr; 
+	texr.x = SCR_WDT/2048;
+	texr.y = SCR_HGT/2048;
+       	texr.w = w;//*2;
+       	texr.h = h;//*2;
+
+	int is_running = 1;
 	
 	SDL_Event event;
       	while (is_running)
@@ -30,28 +49,28 @@ int main ()
         	{
            		if ( event.type == SDL_QUIT )
             		{
-				if (is_running == 1)
-					is_running = 2;
-				else
-					is_running = 0;
+				is_running = 0;
             		}   
        	 	}	
-        	if (is_running == 1)
-		{
-			SDL_RenderClear ( renderer );
-			DrawCircle( renderer, SCR_CEN_X, SCR_CEN_Y, 200);
-			SDL_SetRenderDrawColor ( renderer, 0, 0, 0, 255 );
-        		SDL_RenderPresent ( renderer );
-		}
-		else if (is_running == 2)
-		{
-			SDL_RenderClear (renderer);
-			DrawCircle( renderer, 250, SCR_CEN_Y, 200);
-			SDL_SetRenderDrawColor (renderer, 0, 0, 0, 255);
-			SDL_RenderPresent ( renderer );
-		}
+        	
+		// Clears Screen
+		SDL_RenderClear ( renderer );
+		// Copies Texture to rendering context
+		SDL_RenderCopy(renderer, background, NULL, &texr);
+		
+		// Draws Circle
+		DrawCircle( renderer, SCR_CEN_X, SCR_CEN_Y, 200);
+		
+		SDL_SetRenderDrawColor ( renderer, 0, 0, 0, 255 );
+        	SDL_RenderPresent ( renderer );
+		
 	}
-    	SDL_Quit ();
+
+	SDL_DestroyTexture(background);
+	SDL_DestroyRenderer(renderer);
+    	SDL_DestroyWindow(window);
+	
+	SDL_Quit ();
 	return 0;
 }
 
