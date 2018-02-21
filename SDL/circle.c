d37d0bb165f80934b0facbbc3e385cc6d5818920
DUONG Ngoc Phuong Anh <ngocphuonganh.duong@epita.fr>
2018-02-21 23:33:06 +0100

    moving circle with arrow keys

--- SDL/circle.c
+++ SDL/circle.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL_image.h>
-#include <unistd.h>
 
 #define SCR_WDT 1280
 #define SCR_HGT 960
@@ -29,24 +28,48 @@ int MoveCircle(SDL_Renderer *r,int x, int y, int a, int b, int radius, SDL_Textu
 		return -1;
 	if (b < 0 || b > SCR_HGT)
 		return -1;
-	
-	while (x != a || y != b)
-	{
-		printf("x:%d a:%d\ny:%d b:%d", x, a, y, b);
-		if (x < a)
-			x += 1;
-		if (x > a)
-			x -= 1;
-		if (y < b)
-			y += 1;
-		if (y > b)
-			y -= 1;
-		SDL_RenderClear(r);
-		SDL_RenderCopy(r, bg, NULL, tex);
-		DrawCircle(r, x, y, radius);
-		SDL_SetRenderDrawColor(r, 0,0,0,255);
-		SDL_RenderPresent(r);
-	}
+        int is_running = 1;
+	SDL_Event event;
+	while (is_running)
+	  {
+	    SDL_WaitEvent(&event);	
+	    if (event.type == SDL_QUIT) // close by click x button or push enter
+	      {
+		is_running = 0;
+	      }
+	    if (event.type == SDL_KEYDOWN)
+	      {
+		switch(event.key.keysym.sym)
+		  {
+		  case SDLK_RETURN:
+		    is_running = 0;
+		    break;
+		  case SDLK_LEFT:
+		    if (x > radius)
+		      x -= 1;
+		    break;
+		  case SDLK_RIGHT:
+		    if (x < a-radius)
+		      x += 1;
+		    break;
+		  case SDLK_UP:
+		    if (y > radius)
+		      y -= 1;
+		    break;
+		  case SDLK_DOWN:
+		    if (y < b-radius)
+		      y += 1;
+		  default:
+		    break;
+		  }
+	      }
+	    printf("x:%d a:%d\ny:%d b:%d", x, a, y, b);
+	    SDL_RenderClear(r);
+	    SDL_RenderCopy(r, bg, NULL, tex);
+	    DrawCircle(r, x, y, radius);
+	    SDL_SetRenderDrawColor(r, 0,0,0,255);
+	    SDL_RenderPresent(r);
+	  }	
 	return 1;
 }
 
@@ -57,15 +80,15 @@ int init_circle(int radius)
 	
       	SDL_Init ( SDL_INIT_VIDEO ); 
 
-    	SDL_Window *window = SDL_CreateWindow ( "Kurt Russel's Teapot - StarDwarf", SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0); //SCR_WDT, SCR_HGT, 0 );
+    	SDL_Window *window = SDL_CreateWindow ( "Drawing a Circle", SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED, SCR_WDT, SCR_HGT, 0 );
     	SDL_Renderer *renderer = SDL_CreateRenderer ( window, -1, SDL_RENDERER_SOFTWARE );
    	
 	int w, h;
 
 
 	// Loading image
-	background = IMG_LoadTexture(renderer, "../gui/img/bg.jpg"); //I don't want to display bg for the time being
+	background = IMG_LoadTexture(renderer, "../resources/bg.jpg");
 	SDL_QueryTexture(background, NULL, NULL, &w, &h);
 	SDL_SetRenderTarget(renderer, background);
 
@@ -75,51 +98,21 @@ int init_circle(int radius)
 	texr.y = SCR_HGT/2048;
        	texr.w = w;// *2;
        	texr.h = h;// *2;
-
-	int is_running = 1;
-	int x = 2*radius;
-	int y = 2*radius;
-	SDL_Event event;
+	
       	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, background, NULL, &texr);
 	DrawCircle(renderer, 0+radius, 0, radius);
 	
 	SDL_RenderPresent(renderer);
 	SDL_RenderCopy(renderer, background, NULL, &texr);
-	sleep(3);
-	MoveCircle(renderer, x, y, 1280-2*radius, 720-2*radius, radius, background, &texr);
+	MoveCircle(renderer, 0+radius, 0, SCR_WDT, SCR_HGT, radius, background, &texr);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderPresent(renderer);
-	x = 1280 -2*radius;
-	y = 720-2*radius;
-	while (is_running)
-	{
-		SDL_WaitEvent(&event);	
-	    	if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RETURN)) // close by click x button or push enter
-	      	{
-			is_running = 0;
-	      	}
 
-
-	    	if (event.type == SDL_KEYDOWN)
-		{
-			if(event.key.keysym.sym == SDLK_DOWN)
-	    			y +=1;
-		    	if (event.key.keysym.sym == SDLK_UP)
-				y -=1;
-	    		if (event.key.keysym.sym == SDLK_LEFT)
-				x -=1;
-			if (event.key.keysym.sym == SDLK_RIGHT)
-				x += 1;
-
-
-			SDL_RenderClear(renderer);
-			SDL_RenderCopy(renderer, background, NULL, &texr);
-			
-			DrawCircle(renderer, x, y, radius);
-			SDL_SetRenderDrawColor(renderer, 0,0,0,255);
-			SDL_RenderPresent(renderer);
-		/*// Clears Screen
+	MoveCircle(renderer, 0+radius, 0, SCR_WDT, SCR_HGT, radius, background, &texr);
+  	
+	/*
+		// Clears Screen
 		SDL_RenderClear ( renderer );
 		// Copies Texture to rendering context
 		SDL_RenderCopy(renderer, background, NULL, &texr);
@@ -129,12 +122,12 @@ int init_circle(int radius)
 		//Movecircle(renderer, radius, SCR_CEN_Y, 588, 854, radius);
 		
 		SDL_SetRenderDrawColor ( renderer, 0, 0, 0, 255 );
-        	SDL_RenderPresent ( renderer );*/
-		}
-	}
+        	SDL_RenderPresent ( renderer );
+		*/
+	
 
-	if (is_running)
-		puts("test");
+	//	if (is_running)
+	//	puts("test");
 
 	SDL_DestroyTexture(background);
 	SDL_DestroyRenderer(renderer);
