3d77789df029963158bbaa688c569134b192f1d0
Elie Halimi <elie.halimi@epita.fr>
2018-04-03 14:18:45 +0200

    started creating a save/load system will have to test it and fix errors if any (I am sure there are a few

---
+++ save/save.h
@@ -0,0 +1,36 @@
+#ifnfef _SAVE_SYSTEM_H_
+#define _SAVE_SYSTEM_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "../Physic/physic.h"
+
+void save_system(struct system *sytem) //name if several systems in one simulation 
+{
+	assert(system != NULL);
+
+	FILE *f = fopen("system.txt", "r");
+	assert(f != NULL);
+	
+	fwrite(&system,sizeof(system), sizeof(char), f);
+}
+
+struct system *load_system(char *path)
+{
+	FILE *f = fopen(path, "w+");
+	assert(f != NULL);
+
+	struct system *s = malloc(sizeof(struct system));
+	
+	int i = 0;
+	char buf[1];
+	while( (size_t c = fread(&buf, 1, 1, f)) > 0)
+	{
+		*(s+i);
+		i++;
+	}
+	return s;
+}
+
+#endif
