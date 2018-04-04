c3e66976e4cb92d3f5c2ddeb61fe4345d8cc3479
Elie Halimi <elie.halimi@epita.fr>
2018-04-04 19:51:22 +0200

    fixed saved but still doesn't work correctly

---
+++ save/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "save.h"
+#include "../Physic/physic.h"
+#include "../Physic/libvector.h"
+
+int main()
+{
+
+	struct system *system = new_system(2);
+
+	const float val[2] = {500, 500};
+	const float *values = (const float *) val;
+
+	const struct vector *position = new_vector(2, values);
+	struct item *item = new_item(position);
+	item->size = 200;
+
+	push_item(system, item);
+
+	item->rect = NULL;
+	puts("Drawing");
+	printf("x = %f, y = %f\n", item->position.values[0], item->position.values[1]);
+	puts("Finished Drawing");
+
+	save_system(system);
+	puts("saved");
+
+
+}
