fd63f684253dbdff93aa8ac61450013939d51d6f
Samuel Corno <samuel.corno@epita.fr>
2018-05-20 17:42:30 +0200

    we can now modify an item through the input's values in Edit mode

--- gui/main.c
+++ gui/main.c
@@ -35,6 +35,7 @@ int main()
 {
   SDL_Event e;
   struct system *sys = NULL;
+  struct item *selected = NULL;
   struct system *reset_sys = NULL;
   struct htable *button_list = create_htable(20);
   struct htable *slider_list = create_htable(3);	
@@ -128,7 +129,7 @@ int main()
 	  if (((struct text *)(access_htable(text_list, "name_loadmenu")->value))->active)  
 	    textinput(e,(struct text *)(access_htable(text_list, "name_loadmenu")->value), 25, NULL);
 	  if(sys)
-	    camera_event(sys->camera, &e, &sys->selected);
+	    camera_event(sys->camera, &e, &selected);
 	    
 	}
 		
@@ -137,11 +138,29 @@ int main()
 
       if(sys != NULL)
 	{
-	  if(sys->selected != NULL)
-	      item_to_input(text_list, sys->selected);				
+	  if(reset_sys == sys)
+	    {
+	      if(sys->selected != NULL)
+		item_to_input(text_list, sys->selected);
+	      reset_sys = NULL;
+	    }
+	  
+	  if(selected != NULL)
+	    {
+	      sys->selected = selected;
+	      selected = NULL;
+	      item_to_input(text_list, sys->selected);
+	    }
+
+	  if(simulation_state == SIMULATION_EDIT && sys->selected != NULL)
+	    input_to_item(text_list, sys->selected);
 	   
 	  if(simulation_state == SIMULATION_PROGRESS)
-	    update_system(sys);
+	    {
+	      update_system(sys);
+	      if(sys->selected != NULL)
+		item_to_input(text_list, sys->selected);
+	    }
 			
 	  if(simulation_state == SIMULATION_EDIT)
 	    {
